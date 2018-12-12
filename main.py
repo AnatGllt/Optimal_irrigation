b69625bab97b0269ade858e1ced04202a8398dbb
JacquesLindsay <jacques.lindsay@orange.fr>
2018-12-12 21:06:31 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,60 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
-Created on Mon Nov 26 20:54:38 2018
-
+Created on Sun Dec  9 16:02:28 2018
 @author: admin
 """
 
 import random as rd
-from tree import Node
 import tree
+from tree import Node
+from tree import subdivision
 import sys
+import matplotlib.pyplot as plt
+
 """
 sys.setrecursionlimit(20000)
-
 weight = rd.random()
 P = Node(10*rd.random()-5, 10*rd.random()-5, weight)
 Q = Node(10*rd.random()-5, 10*rd.random()-5, 1 - weight)
-
 O = Node(0, 0, 1, [P,Q])
-
 O.B_star(P,Q)
-
 O.plot()
-
 print(O.Malpha())
-
 O.reset()
-
 O = Node(0, 0, 1, [P,Q])
-
 O.B_grad(P,Q)
-
 O.plot()
-
 print(O.Malpha())
 """
+
 xmin = -10
 ymin = -10
 xmax = 10
 ymax = 10
 
-root = [0,0]
-
 domain = [xmin, ymin, xmax, ymax]
 
-N = 1000
+N = 100
 
 points = []
-
 for i in range(N):
-    points.append(Node((xmax-xmin)*rd.random()+xmin, (ymax-ymin)*rd.random()+ymin, rd.random()*8/N))
+    points.append(Node((xmax-xmin)*rd.random()+xmin, (ymax-ymin)*rd.random()+ymin, rd.random()/N))
 
-O = tree.subdivision(root, points, domain)
+root = Node(0, 0, 0)
+
+O = subdivision(root, points, domain)
+
+#O = tree.chain(root, points)
+
+#O = tree.naive(root, points)
+
+#O = tree.average(root, points)
+
+#O = tree.averagesubdiv(root, points, domain)
 
 O.plot()
 
+plt.clf()
 
 
--- tree.py
+++ tree.py
@@ -1,28 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 26 14:52:39 2018
 
-@author: Anatole Gallouet
-"""
-
+import copy
 import numpy as np
 import numpy.linalg as LA
 import matplotlib.pyplot as plt
-from scipy.optimize import fmin_cg
 
 alpha = 0.5
 
 class Node:
     
-    
-    def __init__(self, x, y, w, children = None):
+    def __init__(self, x, y, w, father = None, children = None):
         """initialize an instance of class Node"""
         self.pos = np.array([x,y])
         self.w = w
+        self.father = father
         if children is None:
             self.children = []
         else:
             self.children = children
+            for child in children:
+                child.father = self
+            if w != sum([P.w for P in children]):
+                raise ValueError("Created node has weight different than the sum of its children's")
+    
+    def copy(self):
+        return Node(self.pos[0], self.pos[1], self.w, self.father, None)
     
     def __str__(self):
         string = "position : (" + str(self.pos[0]) + ", "+ str(self.pos[1]) + ") \n" 
@@ -32,15 +33,30 @@ class Node:
     def add_child(self, node):
         """add child "node" to the list of children of self"""
         self.children.append(node)
+        node.father = self
+        self.w += node.w
+        f = self.father
+        while f is not None:
+            f.w += node.w
+            f = f.father
         
     def remove_child(self, node):
         """remove child "node" to the list of children of self"""
-        self.children.remove(node)
+        try:
+            self.children.remove(node)
+        except:
+            raise ValueError("Trying to remove a child that isn't one")
+        self.w -= node.w
+        f = self.father
+        while f is not None:
+            f.w -= node.w
+            f = f.father
         
     def reset(self):
         for i in self.children:
             self.remove_child(i)
             i.reset()
+    
     
     def angle(self, node1, node2):
         """compute the angle between vector self-node1 and self,node2"""
@@ -77,7 +93,6 @@ class Node:
         
         QM = (np.dot(P-O,Q-O)/np.dot(P-O,P-O)) * (P - O) - (Q - O) 
         PH = (np.dot(P-O,Q-O)/np.dot(Q-O,Q-O)) * (Q - O) - (P - O) 
-    
         cot1 = (k2 - k1 - 1) / (4*k1 - (k2 - k1 - 1)**2)**(1/2)
         cot2 = (k1 - k2 - 1) / (4*k2 - (k1 - k2 - 1)**2)**(1/2)
         
@@ -91,25 +106,23 @@ class Node:
         OQP = nodeQ.angle(self, nodeP)
         OPQ = nodeP.angle(self, nodeQ)
         if (POQ >= theta3):
-            return
+            return False
         elif (OQP >= theta1):
             self.remove_child(nodeP)
             nodeQ.add_child(nodeP)
-            nodeQ.w=self.w
-            return
+            return True
         elif (OPQ >= theta2):
             self.remove_child(nodeQ)
             nodeP.add_child(nodeQ)
-            nodeP.w=self.w
-            return
+            return True
         else:
             B = 2*((1 - t)*R + t * S) - O
-            nodeB = Node(B[0], B[1], self.w, [nodeP, nodeQ])
+            nodeB = Node(B[0], B[1], nodeP.w + nodeQ.w, self, [nodeP, nodeQ])
             self.remove_child(nodeP)
             self.remove_child(nodeQ)
             self.add_child(nodeB)
-            return
-        
+            return True
+
     def B_grad(self, nodeP, nodeQ):
         """ compute B_star the optimal point for irrigation  from self
         to nodeP and nodeQ by the gradient descent method """   
@@ -142,20 +155,18 @@ class Node:
         elif (np.linalg.norm(B-Q)<epsilon):
             self.remove_child(nodeP)
             nodeQ.add_child(nodeP)
-            nodeQ.w=self.w
             return
         elif (np.linalg.norm(B-P)<epsilon):
             self.remove_child(nodeQ)
             nodeP.add_child(nodeQ)
-            nodeP.w=self.w
             return
         else:
-            nodeB = Node(B[0], B[1], self.w, [nodeP, nodeQ])
+            nodeB = Node(B[0], B[1], self.w, self, [nodeP, nodeQ])
             self.remove_child(nodeP)
             self.remove_child(nodeQ)
             self.add_child(nodeB)
         return
-        
+    
     def Malpha(self):
         """ computes Malpha aka the sum to minimize """
         s = 0
@@ -164,31 +175,18 @@ class Node:
             s += i.w**alpha * len
             s += i.Malpha()
         return s
-        
-    
-        
-            
+
 def line(A, B, weight):
     """draw a line between A and B of witdh proportionnal to weight"""
-    plt.plot([A[0],B[0]], [A[1], B[1]], marker = ' ', linewidth = 4*weight, color = '0')
-   
-"""sorts a set of points in a domain according to subdivisions
-
-points is the set of points to sort in the form of nodes
-
-x is the set of horizontal delimiters and y is the set of vertical delimiters
-
-this function returs: the set of points corresponding to the centers of the new subdomains,
-the set of matrices corresponding to the root points in each given domain
-and the set of vectors corresponding to the subdomains"""
-
+    plt.plot([A[0],B[0]], [A[1], B[1]], marker = ' ', linewidth = 15*weight, color = '0')
+    
 def sort(points, x, y):
     subpoints = []
     root = []
     subdomain = []
     for i in range(3):
         for j in range(3):
-            root.append([(x[i+1]+x[i])/2.0,(y[j+1]+y[j])/2.0])
+            root.append(Node((x[i+1]+x[i])/2.0, (y[j+1]+y[j])/2.0, 0))
             subdomain.append([x[i],y[j],x[i+1],y[j+1]])
             sp = []
             for k in points:
@@ -196,169 +194,90 @@ def sort(points, x, y):
                     sp.append(k)
             subpoints.append(sp)
     return subpoints, root, subdomain
-    
-    
-"""For the subdivision method, we have a list of weighted points and we want to
-create in intital transport path
-
-We will thus take the coordienates of the root point
-
-We will also take the list of points, which takes the form of a list of nodes
-
-The first two colums are the coordinates of the point while the third is the weight
-
-The weight of the root point is equal to the sums of the weights of the end points
-
-Finally, we will take the doman vector [xmin, ymin, xmax, ymax]"""
 
 def subdivision(root, points, domain):
     N = len(points)
     if N==1:
-        return Node(root[0], root[1], points[0].w, [points[0]])
+        root.add_child(points[0])
+        return root
     if N==2:
-        O = Node(root[0], root[1], points[0].w + points[1].w, [points[0],points[1]])
-        O.B_star(points[0],points[1])
-        return O
+        root.add_child(points[0])
+        root.add_child(points[1])
+        #root.B_star(points[0],points[1])
+        return root
     n=2
     l=3
     K=l**n
-    if N<=K:
+    if N<K:
         gmax=0
-        i_star=Node(root[0],root[1],0)
-        j_star=Node(root[0],root[1],0)
-        B_star=Node(root[0],root[1],0)
-        for i in points:
-            for j in points:
-                if(i.pos[0] != j.pos[0] and i.pos[1] != j.pos[1]):
-                    O = Node(root[0], root[1], i.w+j.w, [i,j])
+        #flag = False will mean that B_star hasn't changed anything
+        for ind1 in range(N):
+            for ind2 in range(ind1+1,N):
+                i = points[ind1].copy()
+                j = points[ind2].copy()
+                if(i.pos[0] != j.pos[0] or i.pos[1] != j.pos[1]):
+                    O = Node(root.pos[0], root.pos[1], i.w+j.w, None, [i,j])
                     MO = O.Malpha()
                     O.B_star(i,j)
                     MB = O.Malpha()
                     g = MO - MB
                     if g>=gmax:
                         g=gmax
-                        i_star=i
-                        j_star=j
-                        B_star.reset()
-                        if(len(O.children)==1):
-                            B_star=O.children[0]
-                        else:
-                            B_star=O
+                        ind = [ind1,ind2]
+                else:
+                    print("bug")
+        i_star = points[ind[0]]
+        j_star = points[ind[1]]
+        O = Node(root.pos[0], root.pos[1], i_star.w+j_star.w, None, [i_star,j_star])
+        flag = O.B_star(i_star, j_star)
         points.remove(i_star)
         points.remove(j_star)
-        O = subdivision(root, points, domain)
-        O.w += B_star.w
-        if(O.pos[0] != B_star.pos[0] and O.pos[1] != B_star.pos[1]):
-            O.add_child(B_star)
+        B_star = O.children[0]
+        if flag:
+            points.append(B_star)
         else:
-            O.add_child(i_star)
-            O.add_child(j_star)
+            root.add_child(i_star)
+            root.add_child(j_star)
+        return subdivision(root, points, domain)
     xlines=np.linspace(domain[0],domain[2],l+1)
     ylines=np.linspace(domain[1],domain[3],l+1) 
     subpoints, subroot, subdomain = sort(points, xlines, ylines)
-    G=[]
-    w=0
     for i in range(K):
         if(len(subpoints[i]))>0:
             Gi=subdivision(subroot[i], subpoints[i], subdomain[i])
-            w+=Gi.w
-            G.append(Gi)
-    return Node(root[0],root[1],w,G)
-
-""" Subdivision algorithm applied without the method on a small number of points """
-
-def extsubdivision(root, points, domain):
-    N = len(points)
-    if N==1:
-        return Node(root[0], root[1], points[0].w, [points[0]])
-    if N==2:
-        O = Node(root[0], root[1], points[0].w + points[1].w, [points[0],points[1]])
-        O.B_star(points[0],points[1])
-        return O
-    n=2
-    l=3
-    K=l**n
-    xlines=np.linspace(domain[0],domain[2],l+1)
-    ylines=np.linspace(domain[1],domain[3],l+1) 
-    subpoints, subroot, subdomain = sort(points, xlines, ylines)
-    G=[]
-    w=0
-    for i in range(K):
-        if(len(subpoints[i]))>0:
-            Gi=extsubdivision(subroot[i], subpoints[i], subdomain[i])
-            w+=Gi.w
-            G.append(Gi)
-    return Node(root[0],root[1],w,G)
-
-""" Method on a small number of points applied globally"""
-
-def pairing(root, points):
-    N = len(points)
-    if N==1:
-        return Node(root[0], root[1], points[0].w, [points[0]])
-    if N==2:
-        O = Node(root[0], root[1], points[0].w + points[1].w, [points[0],points[1]])
-        O.B_star(points[0],points[1])
-        return O
-    gmax=0
-    i_star=Node(root[0],root[1],0)
-    j_star=Node(root[0],root[1],0)
-    B_star=Node(root[0],root[1],0)
-    for i in points:
-        for j in points:
-            if(i.pos[0] != j.pos[0] and i.pos[1] != j.pos[1]):
-                O = Node(root[0], root[1], i.w+j.w, [i,j])
-                MO = O.Malpha()
-                O.B_star(i,j)
-                MB = O.Malpha()
-                g = MO - MB
-                if g>=gmax:
-                    g=gmax
-                    i_star=i
-                    j_star=j
-                    B_star.reset()
-                    if(len(O.children)==1):
-                        B_star=O.children[0]
-                    else:
-                        B_star=O
-    points.remove(i_star)
-    points.remove(j_star)
-    O = pairing(root, points)
-    O.w += B_star.w
-    if(O.pos[0] != B_star.pos[0] and O.pos[1] != B_star.pos[1]):
-        O.add_child(B_star)
-    else:
-        O.add_child(i_star)
-        O.add_child(j_star)
-    return O
+            root.add_child(Gi)
+    return root
 
 """Chain method, where each node has a child which is the least costly node to it"""
 
 def chain(root, points):
-    w = 0
-    for i in points:
-        w+=i.w
+    if (len(points)==0):
+        return root
     imin = points[0]
-    Omin = Node(root[0], root[1], w, [imin])
-    Mmin = Omin.Malpha()
+    root.add_child(imin)
+    Mmin = root.Malpha()
     for i in points:
-        O = Node(root[0], root[1], w, [i])
-        if (O.Malpha() < Mmin):
-            Mmin = O.Malpha()
+        root.reset()
+        root.add_child(i)
+        if (root.Malpha() < Mmin):
+            Mmin = root.Malpha()
             imin = i
-            Omin = O
+    root.reset()
+    root.add_child(imin)
     points.remove(imin)
-    if (len(points)>0):
-        Omin.add_child(chain(imin.pos, points))
-    return Omin
+    imin = chain(imin, points)
+    return root
 
 """ Naive method"""
 
 def naive(root, points):
     w = 0
+    O = root
     for i in points:
         w+=i.w
-    return Node(root[0], root[1], w, points)
+        O.add_child(i)
+    O.w = w
+    return O
 
 """Average method: averages the weights of all the points and uses that as the
 node that branches from the center to every point"""
@@ -368,44 +287,43 @@ def average(root, points):
     for i in points:
         w+=i.w
         p += i.pos*i.w**alpha
-    p[0] += root[0]*w**alpha
-    p[1] += root[1]*w**alpha
+    p += root.pos*w**alpha
     p /= (len(points)+1)
-    O = Node(p[0], p[1], w, points)
-    return Node(root[0], root[1], w, [O])
-
-"""Average method: averages the weights of all the points and uses that as the
-node that branches from the center to every point"""
-def average(root, points):
-    p = [0,0]
-    w = 0
+    O = Node(p[0], p[1], w)
     for i in points:
-        w+=i.w
-        p += i.pos*i.w**alpha
-    p[0] += root[0]*w**alpha
-    p[1] += root[1]*w**alpha
-    p[0] /= (len(points)+1)
-    p[1] /= (len(points)+1)
-    O = Node(p[0], p[1], w, points)
-    return Node(root[0], root[1], w, [O])
+        O.add_child(i)
+    root.add_child(O)
+    root.w=w
+    return root
 
 """Average method applied to the subdivision method """
 
 def averagesubdiv(root, points, domain):
-    N=len(points)
-    n=2
-    l=3
-    K=l**n
-    if N<=K:
-        return average(root, points)
-    xlines=np.linspace(domain[0],domain[2],l+1)
-    ylines=np.linspace(domain[1],domain[3],l+1) 
-    subpoints, subroot, subdomain = sort(points, xlines, ylines)
-    G=[]
-    w=0
-    for i in range(K):
-        if(len(subpoints[i]))>0:
-            Gi=averagesubdiv(root, subpoints[i], subdomain[i])
-            w+=Gi.w
-            G.append(Gi)
-    return Node(root[0],root[1],w,G)
+    def iteration(points, domain):
+        N=len(points)
+        n=2
+        l=3
+        K=l**n
+        if N<=K:
+            p = [0,0]
+            w = 0
+            for i in points:
+                w+=i.w
+                p += i.pos*i.w**alpha
+            p += root.pos*w**alpha
+            p /= (len(points)+1)
+            O = Node(p[0], p[1], w)
+            for i in points:
+                O.add_child(i)
+            root.add_child(O)
+            root.w +=w
+            return
+        xlines=np.linspace(domain[0],domain[2],l+1)
+        ylines=np.linspace(domain[1],domain[3],l+1) 
+        subpoints, subroot, subdomain = sort(points, xlines, ylines)
+        for i in range(K):
+            if(len(subpoints[i]))>0:
+                iteration(subpoints[i], subdomain[i])
+        return
+    iteration(points, domain)
+    return root
